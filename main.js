ce86c7dcc8de0347c6d56a1c5488adde48576de9
Robin Banks <77512549+robinbanksy@users.noreply.github.com>
2026-02-02 14:11:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const search = document.querySelector(".search");
+const button = document.querySelector(".search-button");
+const sprite = document.querySelector(".sprite");
+
+async function getPokemon(name) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
+    const data = await response.json();
+
+    sprite.src = data.sprites.front_default;
+  
+}
+
+button.addEventListener("click", function () {
+    console.log("Success!");
+    getPokemon(search.value);
+});
+
+sprite.style.width = "200px";
